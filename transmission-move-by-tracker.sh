101c7de0f440b217a271efd085ffef26d06a3ea3
James Turnbull <james@jamesturnbull.org>
2012-01-13 12:21:32 +0000

    Wrapped tracker grep in single-quotes

--- transmission-move-by-tracker.sh
+++ transmission-move-by-tracker.sh
@@ -7,7 +7,7 @@ IDLIST="$($REMOTE -l | tail -n +2 | grep 100% | grep -v Stopped | awk '{ print $
 
 
 for ID in $IDLIST; do
-  TRACKER="$($REMOTE --torrent $ID -it | grep $TRACKER)"
+  TRACKER="$($REMOTE --torrent $ID -it | grep '$TRACKER')"
   if [ $? -eq 0 ]; then
     MOVED="$($REMOTE --torrent $ID -i | grep $DESTDIR)"
     if [ $? -eq 1 ]; then
