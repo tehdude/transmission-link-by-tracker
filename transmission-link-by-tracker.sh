60b419cd428a952654b3303f804dfdb6de79075a
tehdude <mainframe.it@gmail.com>
2013-09-29 16:25:27 +0200

    Update transmission-link-by-tracker.sh

--- transmission-link-by-tracker.sh
+++ transmission-link-by-tracker.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
+#replace the values with yours
 
 REMOTE="transmission-remote localhost"
-DESTDIR="/mnt/sda1/download/music"
-TRACKER="ubuntu\.com"
+TRACKER="trackername"
+DESTDIR="/desiredpath/$TRACKER"
 IDLIST="$($REMOTE -l | tail -n +2 | grep 100% | grep -v Stopped | awk '{ print $1; }')"
 
-
 for ID in $IDLIST; do
-  TRACKER="$($REMOTE --torrent $ID -it | grep '$TRACKER')"
+  TRACK="$($REMOTE --torrent $ID -it | grep $TRACKER)"
   if [ $? -eq 0 ]; then
-    MOVED="$($REMOTE --torrent $ID -i | grep $DESTDIR)"
-    if [ $? -eq 1 ]; then
-      RESULT="$($REMOTE --torrent $ID --move $DESTDIR)"
-      echo "Moved $ID"
-    fi
+    LOCATION="$($REMOTE --torrent $ID -i | grep "Location" | cut -d' ' -f4)"
+    FILEN="$($REMOTE --torrent $ID -if | head -n 1 | cut -d'(' -f1)"
+    LEN=${#FILEN}
+    let LEN--
+    echo $LEN
+    FILEN="$(echo $FILEN | cut -c1-$LEN)"
+    ln -s "$LOCATION$FILEN" "$DESTDIR/$FILEN"
   fi
 done
+
